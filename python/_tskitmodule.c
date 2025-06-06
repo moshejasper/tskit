7920509a34f13e4d284585ac88e94d47d563942b
Moshe Jasper <mjasper@student.unimelb.edu.au>
2025-06-06 12:47:37 +1000

    bugfix in advanced GNN pyinterface

--- python/_tskitmodule.c
+++ python/_tskitmodule.c
@@ -9057,7 +9057,8 @@ TreeSequence_genealogical_nearest_neighbours_advanced(
 
     Py_BEGIN_ALLOW_THREADS err = tsk_treeseq_genealogical_nearest_neighbours_advanced(
         self->tree_sequence, PyArray_DATA(focal_array), num_focal, reference_sets,
-        reference_set_size, num_reference_sets, 0, PyArray_DATA(ret_array));
+        reference_set_size, num_reference_sets, num_neighours, 
+        discrim, 0, PyArray_DATA(ret_array));
     Py_END_ALLOW_THREADS if (err != 0)
     {
         handle_library_error(err);
